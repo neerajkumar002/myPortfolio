dbe915afe2230dfe3f7be9c10b6348cd8b93aa23
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-05-07 18:13:39 +0530

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Project from "./components/Project/Project";
 import Skills from "./components/Skills/Skills";
 import Experience from "./components/Experience/Experience";
 import Contact from "./components/Contact/Contact";
-import Footer from "./components/Footer/footer";
 function App() {
   return (
     <div className="w-full min-h-screen bg-black text-white  ">
@@ -14,7 +13,6 @@ function App() {
       <Skills />
       <Experience />
       <Contact />
-      <Footer />
     </div>
   );
 }
