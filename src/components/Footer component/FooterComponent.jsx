c2db512e325c53e9fbef68888045a15ea3b30043
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-05-07 18:51:26 +0530

    fix: add icons in footer

--- src/components/Footer component/FooterComponent.jsx
+++ src/components/Footer component/FooterComponent.jsx
@@ -1,12 +1,12 @@
- 
+import { GithubIcon, LinkedinIcon } from "lucide-react";
 
 const FooterComponent = () => {
   return (
     <footer className="flex flex-col gap-3  items-center py-5">
       <h1 className="font-semibold text-2xl">Neeraj Kumar </h1>
       <div className="flex gap-3 ">
-        {/* <GithubIcon />
-        <LinkedinIcon /> */}
+        <GithubIcon />
+        <LinkedinIcon />
       </div>
       <p className="text-sm">&copy; 2025 All rights reserved.</p>
     </footer>
