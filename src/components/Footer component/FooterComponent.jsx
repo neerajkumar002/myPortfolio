aaaaca076037c0466f196af984d92fb0e6494dd0
Neeaj Kumar <neerajkumar08697@gmail.com>
2025-05-08 10:57:27 +0530

    add social links

--- src/components/Footer component/FooterComponent.jsx
+++ src/components/Footer component/FooterComponent.jsx
@@ -5,8 +5,12 @@ const FooterComponent = () => {
     <footer className="flex flex-col gap-3  items-center py-5">
       <h1 className="font-semibold text-2xl">Neeraj Kumar </h1>
       <div className="flex gap-3 ">
-        <GithubIcon />
-        <LinkedinIcon />
+        <a href="https://github.com/neerajkumar002" target="_blank">
+          <GithubIcon />
+        </a>
+        <a href="https://www.linkedin.com/in/neerajkumar22/" target="_blank">
+          <LinkedinIcon />
+        </a>
       </div>
       <p className="text-sm">&copy; 2025 All rights reserved.</p>
     </footer>
